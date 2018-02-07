7f1edcfe5ab27687d46f96a4263f45e3f3d3b633
nikersify <nikersifyyt@gmail.com>
2018-02-07 08:51:53 +0100

    test ignore option when .gitignore is not in cwd

--- test/cli.js
+++ test/cli.js
@@ -1,23 +1,22 @@
-import os from 'os'
 import path from 'path'
 
 import execa from 'execa'
-import fs from 'fs-extra'
+import struc from 'struc'
 import test from 'ava'
 
 import pkg from '../package'
 
 import {flagsToOptions, mergeToArr} from '../lib/cli-helpers'
 
-const cwd = path.dirname(__dirname)
-const cli = (args, opts) => execa(path.join(cwd, 'cli.js'), args, opts)
+const moduleRoot = path.dirname(__dirname)
+const cli = (args, opts) => execa(path.join(moduleRoot, 'cli.js'), args, opts)
 
 test('is executable', async t => {
-	const {stdout} = await cli(['--version'], {cwd})
+	const {stdout} = await cli(['--version'])
 	t.is(stdout.indexOf(pkg.name), 0)
 })
 
-test('converts flags to options', async t => {
+test('converts flags to options', t => {
 	t.deepEqual(flagsToOptions({
 		watch: 'a',
 		ignore: 'b',
@@ -40,11 +39,20 @@ test('converts flags to options', async t => {
 		lazy: false
 	})
 
-	const gi = await fs.readFile(path.join(cwd, '.gitignore'), 'utf8')
+	process.chdir(struc({
+		'.gitignore': 'hello\nworld\n'
+	}))
 
 	t.deepEqual(flagsToOptions({}), {
 		watch: ['.'],
-		ignore: gi.split(os.EOL).filter(x => x.length > 0)
+		ignore: ['hello', 'world']
+	})
+
+	process.chdir(struc({}))
+
+	t.deepEqual(flagsToOptions({}), {
+		watch: ['.'],
+		ignore: []
 	})
 })
 
